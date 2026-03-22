2eaefc27b9a7b49aba57a5838b2f151b03cb5367
nwwr9235 <nwwr9235@gmail.com>
2026-03-23 00:10:10 +0300

    Update __init__.py

--- bot/keyboard/__init__.py
+++ bot/keyboard/__init__.py
@@ -1,1 +1,7 @@
+from bot.keyboard.inline import (
+    captcha_keyboard,
+    confirm_keyboard,
+    admin_panel_keyboard
+)
 
+__all__ = ['captcha_keyboard', 'confirm_keyboard', 'admin_panel_keyboard']
