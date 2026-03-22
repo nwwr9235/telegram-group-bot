e802821d3f176d230f6bddf740e8efd1ff1cdbaf
nwwr9235 <nwwr9235@gmail.com>
2026-03-23 00:09:31 +0300

    Update __init__.py

--- bot/handlers/__init__.py
+++ bot/handlers/__init__.py
@@ -4,4 +4,10 @@ from bot.handlers.captcha import router as captcha_router
 from bot.handlers.owner import router as owner_router
 from bot.handlers.group_events import router as group_events_router
 
-__all__ = ['commands_router', 'admin_text_router', 'captcha_router', 'owner_router', 'group_events_router']
+__all__ = [
+    'commands_router',
+    'admin_text_router',
+    'captcha_router',
+    'owner_router',
+    'group_events_router'
+]
